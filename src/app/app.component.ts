30a709d7765fe1c88e1f2dedcf4473f8d0ff60f9
Anita Gasteiner <anita.gasteiner@posteo.de>
2025-06-12 11:51:24 +0200

    firebase prepared

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -6,6 +6,9 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatDividerModule } from '@angular/material/divider';
+import { collection, collectionData, Firestore } from '@angular/fire/firestore';
+import { AsyncPipe } from '@angular/common';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -17,11 +20,22 @@ import { MatDividerModule } from '@angular/material/divider';
     MatIconModule,
     MatButtonModule,
     MatTooltipModule,
-    MatDividerModule
+    MatDividerModule,
+    AsyncPipe
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+
   title = 'simple-crm';
+
+  firestore: Firestore = inject(Firestore);
+  users$: Observable<any[]>;
+
+  constructor() {
+    const aCollection = collection(this.firestore, 'users');
+    this.users$ = collectionData(aCollection);
+  }
+
 }
