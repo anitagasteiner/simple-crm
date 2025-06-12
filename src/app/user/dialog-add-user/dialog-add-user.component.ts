515d0552d3ba2cfc3144314ac147cb414db6989d
Anita Gasteiner <anita.gasteiner@posteo.de>
2025-06-12 09:51:16 +0200

    add user component

--- src/app/user/dialog-add-user/dialog-add-user.component.ts
+++ src/app/user/dialog-add-user/dialog-add-user.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, inject, model } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogActions, MatDialogClose, MatDialogContent, MatDialogRef, MatDialogTitle } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-dialog-add-user',
-  imports: [],
-  templateUrl: './dialog-add-user.component.html',
+  providers: [provideNativeDateAdapter()],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogActions,
+    MatDialogClose,
+    MatDatepickerModule
+  ],
+  templateUrl: './dialog-add-user.component.html',  
   styleUrl: './dialog-add-user.component.scss'
 })
 export class DialogAddUserComponent {
+
+  readonly dialogRef = inject(MatDialogRef<DialogAddUserComponent>);
+  readonly data = {
+    animal: '',
+    name: ''
+  }
+  readonly animal = model(this.data.animal);
+  name: any;
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject, model, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogAddUserComponent } from './dialog-add-user/dialog-add-user.component';
 
 @Component({
   selector: 'app-user',
@@ -10,15 +15,30 @@ import { MatDividerModule } from '@angular/material/divider';
     MatButtonModule,
     MatIconModule,
     MatTooltipModule,
-    MatDividerModule
+    MatDividerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule
   ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
 
-  openDialogAddUser() {
-    
+  readonly animal = signal('');
+  readonly name = model('');
+  readonly dialog = inject(MatDialog);
+
+  openDialogAddUser(): void {
+    const dialogRef = this.dialog.open(DialogAddUserComponent, {
+      data: {name: this.name(), animal: this.animal()},
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed.');
+      if (result !== undefined) {
+        this.animal.set(result);
+      }
+    });
   }
 
 }
