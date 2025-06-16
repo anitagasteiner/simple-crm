c2859732d3dcf8d011522648947909008c391073
Anita Gasteiner <anita.gasteiner@posteo.de>
2025-06-16 15:40:06 +0200

    user details prepared

--- src/app/user/user-details/user-details.component.ts
+++ src/app/user/user-details/user-details.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-user-details',
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatButtonModule
+  ],
   templateUrl: './user-details.component.html',
   styleUrl: './user-details.component.scss'
 })
